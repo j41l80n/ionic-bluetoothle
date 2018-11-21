968c3472fabe7c692d2df413949d684bb6859d26
Jailson Santos <jailson_donald_cobain@hotmail.com>
2018-11-20 23:00:45 -0300

    resolvendo dependencias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+// 
+import { BluetoothLE } from '@ionic-native/bluetooth-le';
+// 
 @NgModule({
   declarations: [
     MyApp,
@@ -24,7 +27,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    BluetoothLE
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+// 
+import { Platform } from 'ionic-angular';
+import { BluetoothLE } from '@ionic-native/bluetooth-le';
+// 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public bluetoothle: BluetoothLE, public plt: Platform,  public navCtrl: NavController) {
+    this.plt.ready().then((readySource) => {
 
+      console.log('Platform ready from', readySource);
+   
+      this.bluetoothle.initialize().then(ble => {
+        console.log('ble', ble.status) // logs 'enabled'
+      });
+   
+     });
   }
 
 }
